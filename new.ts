468f52b0c486deed62e374e58416093d7759e974
Raihan Ahmed <raihan.tusher@yahoo.com>
2019-09-23 19:57:53 +0600

    new 3

--- new.ts
+++ new.ts
@@ -1,19 +1,4 @@
-class Animal {
-    move(distanceInMeters: number = 0) {
-        console.log(`Animal moved ${distanceInMeters}m.`);
-    }
-}
 
-class Dog extends Animal {
-    bark() {
-        console.log('Woof! Woof!');
-    }
-}
-
-const dog = new Dog();
-dog.bark();
-dog.move(10);
-dog.bark();
 let news:string;
 news="wow";
 console.log(news);
